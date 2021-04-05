f1090d2de34a0fe484ff3552425a8dd80a9bcada
Kosovets Maksim <moskvichmax@gmail.com>
2021-04-05 23:33:31 +0300

    front 15

--- src/views/ContactsPage/ContactsPage.js
+++ src/views/ContactsPage/ContactsPage.js
@@ -5,19 +5,19 @@ import styles from './ContactsPage.module.css'
 const ContactsPage = () => {
     return (
        <>
-            <h2 className={styles.OutTeam} >Our team</h2>
-            <hr className={styles.hr}  />
-            <div className={styles.ContactsPageList}>
-                <ContactCard />
-                <ContactCard />
-                <ContactCard />
-                <ContactCard />
-                <ContactCard />
-                <ContactCard />
-                <ContactCard />
-                <ContactCard />
-                <ContactCard />
-            </div>
+        <h2 className={styles.OutTeam} >Our team</h2>
+        <hr className={styles.hr}  />
+        <div className={styles.ContactsPageList}>
+            <ContactCard />
+            <ContactCard />
+            <ContactCard />
+            <ContactCard />
+            <ContactCard />
+            <ContactCard />
+            <ContactCard />
+            <ContactCard />
+            <ContactCard />
+        </div>
        </>
     ) 
 }
