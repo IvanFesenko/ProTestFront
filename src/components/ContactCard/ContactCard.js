963e8dc0ed748e2023f6d4a58195533abf85547e
Kosovets Maksim <moskvichmax@gmail.com>
2021-04-05 22:44:51 +0300

    Front 16 Contact Card

--- src/components/ContactCard/ContactCard.js
+++ src/components/ContactCard/ContactCard.js
@@ -4,18 +4,18 @@ import img from './../../assets/images/Rectangle_17.png'
 
 const ContactCard = () => {
     return (
-            <div className={styles.ContactsCardList}>
-                 <div className={styles.ContactsCard}>
-                    <img src={img} alt="name" />
-                    <div>
-                        <h5>Name</h5>
-                        <span>Front-End Developer </span>
-                        <p>
-                            Lorem Ipsum has been the standard "fish" for Latin texts since the early 16th century.
-                        </p>
-                    </div>
-                 </div>
+        <div className={styles.ContactsCardList}>
+            <div className={styles.ContactsCard}>
+                <img src={img} alt="name" />
+                <div>
+                    <h5>Name</h5>
+                    <span>Front-End Developer </span>
+                    <p>
+                        Lorem Ipsum has been the standard "fish" for Latin texts since the early 16th century.
+                    </p>
+                </div>
             </div>
+        </div>
     ) 
 }
 
