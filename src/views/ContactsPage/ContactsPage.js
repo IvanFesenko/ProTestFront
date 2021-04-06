82d2029e5f7225e7049dc19ae78109573cb826d6
Kosovets Maksim <moskvichmax@gmail.com>
2021-04-06 09:56:30 +0300

    front 15

--- src/views/ContactsPage/ContactsPage.js
+++ src/views/ContactsPage/ContactsPage.js
@@ -1,22 +1,31 @@
 import ContactCard from './../../components/ContactCard'
-
 import styles from './ContactsPage.module.css'
+import img from './../../assets/images/Rectangle_17.png'
+
+
+const contacts = [
+    {id:0, img, title:'Name 1', position:'Front-End Developer', description:'Lorem Ipsum has been the standard "fish" for Latin texts since the early 16th century.' },
+    {id:1, img, title:'Name 2', position:'Front-End Developer', description:'Lorem Ipsum has been the standard "fish" for Latin texts since the early 16th century.' },
+    {id:2, img, title:'Name 3', position:'Front-End Developer', description:'Lorem Ipsum has been the standard "fish" for Latin texts since the early 16th century.' },
+    {id:3, img, title:'Name', position:'Front-End Developer', description:'Lorem Ipsum has been the standard "fish" for Latin texts since the early 16th century.' },
+    {id:4, img, title:'Name', position:'Front-End Developer', description:'Lorem Ipsum has been the standard "fish" for Latin texts since the early 16th century.' },
+    {id:5, img, title:'Name', position:'Front-End Developer', description:'Lorem Ipsum has been the standard "fish" for Latin texts since the early 16th century.' },
+    {id:6, img, title:'Name', position:'Front-End Developer', description:'Lorem Ipsum has been the standard "fish" for Latin texts since the early 16th century.' },
+    {id:7, img, title:'Name', position:'Front-End Developer', description:'Lorem Ipsum has been the standard "fish" for Latin texts since the early 16th century.' },
+    {id:8, img, title:'Name', position:'Front-End Developer', description:'Lorem Ipsum has been the standard "fish" for Latin texts since the early 16th century.' },
+]
+
 
 const ContactsPage = () => {
     return (
        <>
             <h2 className={styles.OutTeam} >Our team</h2>
-            <hr className={styles.hr}  />
             <div className={styles.ContactsPageList}>
-                <ContactCard />
-                <ContactCard />
-                <ContactCard />
-                <ContactCard />
-                <ContactCard />
-                <ContactCard />
-                <ContactCard />
-                <ContactCard />
-                <ContactCard />
+
+                {
+                    contacts.map((contact, k)=>(<ContactCard key={k} {...contact}/>))
+                }
+
             </div>
        </>
     ) 
