6747a49254f477ada660515c22698819a0f872cb
Ivan Fesenko <pharmivan@gmail.com>
2021-04-12 10:16:48 +0300

    diagram fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,14 +2,8 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { Header, Footer, Main, Home, Questions } from 'components';
 import { AuthPage, ContactsPage, UseFulPage } from 'views';
-import ResultsChartPie from 'components/ResultsChartPie';
 import { useSelector } from 'react-redux';
 import { getQuestionsName } from 'redux/questions/questionsSelector';
-
-const data = [
-  { label: 'Correct ', value: 67, color: '#FF6B01' },
-  { label: 'Incorrect  ', value: 20, color: '#D7D7D7' },
-];
 
 function App() {
   const typeTest = useSelector(getQuestionsName);
@@ -18,7 +12,6 @@ function App() {
     <>
       <Header />
       <Main>
-        <ResultsChartPie dataPoints={data} />
         {typeTest && <Questions />}
         <Route exact path="/">
           <Home />
--- src/components/ResultsChartPie/ResultsChartPie.js
+++ src/components/ResultsChartPie/ResultsChartPie.js
@@ -1,169 +1,134 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef } from 'react';
 
+import { isMobileDevice, isTabletDevice } from 'services/utils';
 
-const INITIAL_SCREEN_SIZE = {
-    width: undefined,
-    height: undefined,
+const useCanvasSize = () => {
+  if (isMobileDevice) {
+    return { canvasWidth: 280, canvasHeight: 530 };
   }
-
-const INITIAL_CANVAS_WIDTH = 800
-const INITIAL_CANVAS_HEIDHT = 280
-
-const calculatePercent = (value, total) => {
-        
-    return (value / total * 100).toFixed(0);
+  if (isTabletDevice) {
+    return { canvasWidth: 350, canvasHeight: 530 };
+  }
+  return { canvasWidth: 800, canvasHeight: 280 };
 };
 
-const getTotal = (data) => {
-    let sum = 0;
-    data.forEach(({value}) =>{
-        sum += value;
-    })
-          
-    return sum;
+const calculatePercent = (value, total) => {
+  return ((value / total) * 100).toFixed(0);
+};
+
+const getTotal = data => {
+  let sum = 0;
+  data.forEach(({ value }) => {
+    sum += value;
+  });
+
+  return sum;
 };
 
 const calculateStart = (data, index, total) => {
-    if(index === 0) {
-            return 0;
-    }
-
-    return calculateEnd(data, index-1, total);
+  if (index === 0) return 0;
+  return calculateEnd(data, index - 1, total);
 };
 
 const calculateEndAngle = (data, index, total) => {
-    const angle = data[index].value / total * 360;
-    const inc = ( index === 0 ) ? 0 : calculateEndAngle(data, index-1, total);
-
-    return ( angle + inc );
+  const angle = (data[index].value / total) * 360;
+  const inc = index === 0 ? 0 : calculateEndAngle(data, index - 1, total);
+  return angle + inc;
 };
 
 const calculateEnd = (data, index, total) => {
-
-    return degreeToRadians(calculateEndAngle(data, index, total));
+  return degreeToRadians(calculateEndAngle(data, index, total));
 };
 
-const degreeToRadians = (angle) => {
-    return angle * Math.PI / 180
-}
+const degreeToRadians = angle => {
+  return (angle * Math.PI) / 180;
+};
 
-const calculateSlice = (value, total) =>{
-    return 2 * Math.PI * value / total;
-}
+const calculateSlice = (value, total) => {
+  return (2 * Math.PI * value) / total;
+};
 
-const ResultsChartPie = ({dataPoints}) => {
-    const canvasRef = useRef(null)
+// const dataPoints = [
+//   { label: 'Correct ', value: 67, color: '#FF6B01' },
+//   { label: 'Incorrect  ', value: 20, color: '#D7D7D7' },
+// ];
 
-    const [canvasWidth, setCanvasWidth] =  useState(INITIAL_CANVAS_WIDTH)
-    const [canvasHeight, setCanvasHeight] = useState(INITIAL_CANVAS_HEIDHT)
-    const [screenSize, setScreenSize] = useState(INITIAL_SCREEN_SIZE);
+const ResultsChartPie = ({ dataPoints }) => {
+  const canvasRef = useRef(null);
+  const { canvasWidth, canvasHeight } = useCanvasSize();
 
-    useEffect(()=>{
-        const handleResize = evt =>{
-            setScreenSize({
-                width: window.screen.width,
-                height: window.screen.height,
-              })
-        }
-    
-        window.addEventListener("resize", handleResize);
-        handleResize()
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
 
-    return ()=>{
-        window.removeEventListener("resize", handleResize);
-    }
-    }, [])
+    const x = canvas.width / 2;
+    const y = canvas.height / 2;
+    let startAngle,
+      endAngle,
+      sliceAngle,
+      total = getTotal(dataPoints);
 
+    // Clear ALL
+    context.restore();
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
-    useEffect(()=>{
-        const {width:w} = screenSize
+    dataPoints.forEach(({ label, value, color }, i) => {
+      startAngle = calculateStart(dataPoints, i, total);
+      endAngle = calculateEnd(dataPoints, i, total);
+      sliceAngle = calculateSlice(value, total);
 
-        if(400<=w && w <= 800){
-            setCanvasWidth(350)
-            setCanvasHeight(530)
-        }else if(w<=400){
-            setCanvasWidth(280)
-            setCanvasHeight(530)
-        }else{
-            setCanvasWidth(INITIAL_CANVAS_WIDTH)
-            setCanvasHeight(INITIAL_CANVAS_HEIDHT)
-        }
- 
-    }, [screenSize])
+      const rowLabel = 40 * (i + 1);
 
-    useEffect(() => {
-        const canvas = canvasRef.current
-        const context = canvas.getContext('2d')
+      context.fillStyle = color;
+      context.beginPath();
+      context.moveTo(x, y);
+      context.arc(x, y, x < y ? x : y, startAngle, endAngle, false);
+      context.fill();
 
-        const x = canvas.width / 2;
-        const y = canvas.height / 2;
-        let startAngle,
-            endAngle,
-            sliceAngle,
-            total = getTotal(dataPoints);
+      context.rect(canvas.width - 200, rowLabel, 25, 25); //
+      context.fill();
+    });
 
-        // Clear ALL
-        context.restore();
-        context.clearRect(0, 0, canvas.width, canvas.height);
+    // Line and Point, Label
+    dataPoints.forEach(({ label, value, color }, i) => {
+      startAngle = calculateStart(dataPoints, i, total);
+      endAngle = calculateEnd(dataPoints, i, total);
+      sliceAngle = calculateSlice(value, total);
 
-        dataPoints.forEach(({label, value, color}, i) => {
-            startAngle = calculateStart(dataPoints, i, total);
-            endAngle = calculateEnd(dataPoints, i, total);
-            sliceAngle = calculateSlice(value, total);
+      const pieRadius = Math.min(x, y);
+      const labelX =
+        x + (pieRadius / 2) * Math.cos(startAngle + sliceAngle / 2);
+      const labelY =
+        y + (pieRadius / 2) * Math.sin(startAngle + sliceAngle / 2);
 
-            const rowLabel = 40*(i+1)
+      const rowLabel = 40 * (i + 1);
 
-            context.fillStyle = color;
-            context.beginPath();
-            context.moveTo(x, y);
-            context.arc(x, y, x<y? x : y, startAngle, endAngle, false);
-            context.fill();
+      if (value !== 0) {
+        context.fillStyle = 'transparent';
+        context.beginPath();
+        context.moveTo(canvas.width - 210, rowLabel + 10); //
+        context.lineTo(canvas.width - 260, rowLabel + 10); //
+        context.lineTo(labelX, labelY);
+        context.stroke();
+        context.fill();
 
-            context.rect(canvas.width - 200, rowLabel, 25, 25);//
-            context.fill();
+        context.fillStyle = '#fff';
+        context.beginPath();
+        context.arc(labelX, labelY, 5, 0, 2 * Math.PI); //
+        context.fill();
+      }
 
-        }); 
+      context.font = '16px sans-serif';
+      context.fillStyle = '#000';
+      context.fillText(
+        `${calculatePercent(value, total)}% ${label}`,
+        canvas.width - 200 + 40,
+        rowLabel + 20,
+      );
+    });
+  }, [dataPoints]);
 
+  return <canvas ref={canvasRef} width={canvasWidth} height={canvasHeight} />;
+};
 
-        // Line and Point, Label
-        dataPoints.forEach(({label, value, color}, i) => {
-            startAngle = calculateStart(dataPoints, i, total);
-            endAngle = calculateEnd(dataPoints, i, total);
-            sliceAngle = calculateSlice(value, total);
-
-            const pieRadius = Math.min(x,y);
-            const labelX = x + (pieRadius / 2) * Math.cos(startAngle + sliceAngle/2);
-            const labelY = y + (pieRadius / 2) * Math.sin(startAngle + sliceAngle/2);
-
-            const rowLabel = 40*(i+1)
-
-            if(value !== 0){
-                context.fillStyle = 'transparent';
-                context.beginPath();
-                context.moveTo(canvas.width - 210, rowLabel+10);//
-                context.lineTo(canvas.width - 260, rowLabel+10); //
-                context.lineTo(labelX, labelY);
-                context.stroke();
-                context.fill();
-
-                context.fillStyle = '#fff';
-                context.beginPath();
-                context.arc(labelX, labelY, 5, 0, 2 * Math.PI);//
-                context.fill();
-            }
-
-            context.font = "16px sans-serif";
-            context.fillStyle = "#000";
-            context.fillText( `${calculatePercent(value, total)}% ${label}`, canvas.width - 200 + 40, rowLabel+20);
-            
-        }); 
-
-    }, [dataPoints, screenSize])
-
-
-    return (
-        <canvas ref={canvasRef} width={canvasWidth} height={canvasHeight} />   
-    )
-}
-
-export default ResultsChartPie
+export default ResultsChartPie;
