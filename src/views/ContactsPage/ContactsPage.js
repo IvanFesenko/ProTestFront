369dc03bf45c7ca1afa8aeef9c876cee2b6187bd
Maxim Kozlov <Maximusvin@gmail.com>
2021-04-13 14:09:35 +0300

    fix card our team

--- src/views/ContactsPage/ContactsPage.js
+++ src/views/ContactsPage/ContactsPage.js
@@ -1,18 +1,18 @@
 import { ContactCard, SectionTitle } from 'components';
-import { List } from './ContactsPage.style';
+import { List, ContactsPageWrap } from './ContactsPage.style';
 
 import contacts from './data';
 
 const ContactsPage = () => {
   return (
-    <>
+    <ContactsPageWrap>
       <SectionTitle>Our team</SectionTitle>
       <List>
         {contacts.map((contact, k) => (
           <ContactCard key={k} {...contact} />
         ))}
       </List>
-    </>
+    </ContactsPageWrap>
   );
 };
 
--- src/views/ContactsPage/ContactsPage.style.js
+++ src/views/ContactsPage/ContactsPage.style.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
 
+export const ContactsPageWrap = styled.div`
+  padding-top: 30px;
+  padding-bottom: 50px;
+`;
+
 export const List = styled.ul`
   display: inline-flex;
   flex-wrap: wrap;
   justify-content: center;
-  margin-bottom: 65px;
   transition: 0.45s;
 `;
