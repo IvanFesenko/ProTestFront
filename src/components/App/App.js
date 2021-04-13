d9ae16249d153b6ccd2ffd8e5e0f1d3475c03882
Anastasiia Yermakova <yermak1037@gmail.com>
2021-04-13 13:35:37 +0300

    Add private/public routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,34 +1,65 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import { Header, Footer, Main, Home, Questions } from 'components';
+import { Suspense, useEffect, lazy } from 'react';
+import { Switch, useLocation } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
+import {
+  Header,
+  Footer,
+  Main,
+  Home,
+  Questions,
+  LoaderComponent,
+} from 'components';
 import { AuthPage, ContactsPage, UseFulPage } from 'views';
-import { useSelector } from 'react-redux';
 import { getQuestionsName } from 'redux/questions/questionsSelector';
+import authOperations from 'redux/auth/authOperations';
+import PrivateRoute from 'components/PrivateRoute';
+import PublicRoute from 'components/PublicRoute';
 
 function App() {
   const typeTest = useSelector(getQuestionsName);
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  useEffect(() => {
+    dispatch(authOperations.getCurrentUser());
+  }, [dispatch]);
 
   return (
     <>
       <Header />
       <Main>
-        {typeTest && <Questions />}
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/auth">
-          <AuthPage auth={true} />
-        </Route>
-        <Route path="/register">
-          <AuthPage auth={false} />
-        </Route>
+        <Suspense fallback={<LoaderComponent />}>
+          <Switch>
+            <PrivateRoute exact path="/" redirectTo="/auth">
+              <Home />
+            </PrivateRoute>
 
-        <Route path="/contacts">
-          <ContactsPage />
-        </Route>
-        <Route path="/useful-info">
-          <UseFulPage />
-        </Route>
+            <PublicRoute
+              path="/auth"
+              restricted
+              redirectTo={location?.state?.from ?? '/'}
+            >
+              <AuthPage auth={true} />
+            </PublicRoute>
+
+            <PublicRoute path="/register" restricted redirectTo="/">
+              <AuthPage auth={false} />
+            </PublicRoute>
+
+            <PublicRoute path="/contacts">
+              <ContactsPage />
+            </PublicRoute>
+
+            <PrivateRoute path="/test" redirectTo="/auth">
+              {typeTest && <Questions />}
+            </PrivateRoute>
+
+            <PrivateRoute path="/useful-info" redirectTo="/auth">
+              <UseFulPage />
+            </PrivateRoute>
+          </Switch>
+        </Suspense>
       </Main>
       <Footer />
     </>
