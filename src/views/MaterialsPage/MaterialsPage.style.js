354cc24366ef5f977f6debb2bba4f1f7c634dd56
Ivan Fesenko <pharmivan@gmail.com>
2021-04-13 10:20:14 +0300

    Materials page PC style added

--- src/views/MaterialsPage/MaterialsPage.style.js
+++ src/views/MaterialsPage/MaterialsPage.style.js
@@ -2,6 +2,9 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   padding: 40px 0 110px 0;
+  @media (min-width: 750px) {
+    padding: 120px 0 410px 0;
+  }
 `;
 
 export const Title = styled.h2`
@@ -19,6 +22,9 @@ export const Title = styled.h2`
     top: 13px;
     transform: translateY(-50%);
     position: relative;
+    @media (min-width: 750px) {
+      top: 22px;
+    }
   }
   @media (min-width: 750px) {
     font-size: 22px;
@@ -36,15 +42,25 @@ export const List = styled.ul`
   margin-bottom: 40px;
   padding-left: 20px;
   max-width: 210px;
+  @media (min-width: 750px) {
+    max-width: 295px;
+  }
+  @media (min-width: 900px) {
+    max-width: 505px;
+    padding-left: 230px;
+  }
 `;
 
 export const ListItem = styled.li`
   font-family: 'Montserrat';
   font-size: 10px;
-  font-weight: 500;
   line-height: 16px;
   letter-spacing: 0.02em;
   padding-left: 5px;
+  @media (min-width: 750px) {
+    font-size: 14px;
+    line-height: 22px;
+  }
 `;
 
 export const Link = styled.a`
